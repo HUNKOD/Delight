e850f4d41947979e6bc1a3bae075d670a2edb4a6
HUNKO <Toten1Kopfe@gmail.com>
2022-06-17 16:04:05 +0200

    21

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ window.onload = function() {
           preloader.classList.add('preloader-hidden');
     }, 1200);
 }
-
-// let KUH1 = document.querySelector("#K1");
-// let BAR1 = document.querySelector("#B1");
-// let BAR2 = document.querySelector("#B2");
-
-// function VK1() {
-//     KUH1.classList.remove("VANISH");
-//     BAR1.classList.add("VANISH");
-//     BAR2.classList.add("VANISH");
-//   }
-// function VB1() {
-//     BAR2.classList.remove("VANISH");
-//     KUH1.classList.add("VANISH");
-//     BAR2.classList.add("VANISH");
-    
-// }
-// function VB2() {
-//     BAR2.classList.remove("VANISH");
-//     KUH1.classList.add("VANISH");
-//     BAR1.classList.add("VANISH");
-    
-// }
