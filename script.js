54e5e1ff932e6b8e0062525a57fd331719df1e5f
HUNKO <Toten1Kopfe@gmail.com>
2022-06-23 20:09:56 +0200

    7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// прелоадер
 window.onload = function() {
     let preloader = document.getElementById('preloader');
     preloader.classList.add('hide-preloader');
@@ -5,3 +7,16 @@ window.onload = function() {
           preloader.classList.add('preloader-hidden');
     }, 1200);
 }
+
+
+// апбатон
+setInterval(() => {
+    if (window.pageYOffset < window.innerHeight){
+    document.querySelector('#upbutton').style.opacity = 0;
+    document.querySelector('#upbutton').style.visibility = 'hidden';
+}
+else{
+    document.querySelector('#upbutton').style.opacity = 1;
+    document.querySelector('#upbutton').style.visibility = 'visible';
+}
+}, 500);
