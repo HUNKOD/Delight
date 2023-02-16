1d1e46f3937f9430083e06f703b5a46e19947dc6
HUNKO <Toten1Kopfe@gmail.com>
2023-02-16 21:27:16 +0100

    ой
    This reverts commit 6fa560338ecf1cd14a7400805b2fd4038870a234.

--- script.js
+++ script.js
@@ -7,47 +7,16 @@ window.onload = function() {
           preloader.classList.add('preloader-hidden');
     }, 1200);
 }
-// переход
-document.addEventListener("DOMContentLoaded", function() {
-    showKuhnya();
-  });
-let containerKuhnya = document.querySelector(".container-kuhnya");
-let containerNapoi = document.querySelector(".container-napoi");
-let containerKokteli = document.querySelector(".container-kokteli");
-let buttonKuhnya = document.getElementById('topnav-button-kuhnya');
-let buttonNapoi = document.getElementById('topnav-button-napoi');
-let buttonKokteli = document.getElementById('topnav-button-kokteli');
-function showKuhnya() {
-    containerKuhnya.style.display = "block";
-    containerNapoi.style.display = "none";
-    containerKokteli.style.display = "none";
-    buttonKuhnya.classList.add("whiteAndBlack");
-    buttonNapoi.classList.add("blackAndWhite");
-    buttonKokteli.classList.add("blackAndWhite");
-    buttonKuhnya.classList.remove("blackAndWhite");
-    buttonNapoi.classList.remove("whiteAndBlack");
-    buttonKokteli.classList.remove("whiteAndBlack");
-  }
-function showNapoi() {
-    containerKuhnya.style.display = "none";
-    containerNapoi.style.display = "block";
-    containerKokteli.style.display = "none";
-    buttonKuhnya.classList.add("blackAndWhite");
-    buttonNapoi.classList.add("whiteAndBlack");
-    buttonKokteli.classList.add("blackAndWhite");
-    buttonKuhnya.classList.remove("whiteAndBlack");
-    buttonNapoi.classList.remove("blackAndWhite");
-    buttonKokteli.classList.remove("whiteAndBlack");
-  }
-function showKokteli() {
-    containerKuhnya.style.display = "none";
-    containerNapoi.style.display = "none";
-    containerKokteli.style.display = "block";
-    buttonKuhnya.classList.add("blackAndWhite");
-    buttonNapoi.classList.add("blackAndWhite");
-    buttonKokteli.classList.add("whiteAndBlack");
-    buttonKuhnya.classList.remove("whiteAndBlack");
-    buttonNapoi.classList.remove("whiteAndBlack");
-    buttonKokteli.classList.remove("blackAndWhite");
-  }
-  
+
+
+// апбатон
+// setInterval(() => {
+//     if (window.pageYOffset < window.innerHeight){
+//     document.querySelector('#upbutton').style.opacity = 0;
+//     document.querySelector('#upbutton').style.visibility = 'hidden';
+// }
+// else{
+//     document.querySelector('#upbutton').style.opacity = 1;
+//     document.querySelector('#upbutton').style.visibility = 'visible';
+// }
+// }, 500);
